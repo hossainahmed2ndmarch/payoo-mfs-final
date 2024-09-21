c8d4d610813877874de0b15226b8b033655c5d2b
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2024-09-21 21:11:00 +0600

    Finished the features part

---
+++ js/features.js
@@ -0,0 +1,8 @@
+document.getElementById('add-money-form-show')
+ .addEventListener('click', function () {  
+  showSectionById('add-money-form');
+ });
+document.getElementById('cash-out-form-show')
+ .addEventListener('click', function () {
+  showSectionById('cash-out-form');
+ });
--- js/utilities.js
+++ js/utilities.js
@@ -9,3 +9,14 @@ function getTextFieldById(id) {
  const textNumber = parseFloat(textValue);
  return textNumber;
 }
+
+function showSectionById(id) {
+ document.getElementById('add-money-form').classList.add('hidden');
+ document.getElementById('cash-out-form').classList.add('hidden');
+ // document.getElementById('transfer-money-form').classList.add('hidden');
+ // document.getElementById('get-bonus-form').classList.add('hidden');
+ // document.getElementById('pay-bill-form').classList.add('hidden');
+ // document.getElementById('transactions-log').classList.add('hidden');
+
+ document.getElementById(id).classList.remove('hidden');
+}
